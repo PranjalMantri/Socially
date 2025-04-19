480293dea71892be0b8c869ad4d358cb8931c0ee
Pranjal Mantri <pranjalmantri@gmail.com>
2025-04-19 20:53:27 +0530

    feat: add getDbUserId function for retrieving user ID by Clerk ID

--- actions/user.action.ts
+++ actions/user.action.ts
@@ -57,3 +57,17 @@ export async function getUserByClerkId(clerkId: string) {
 
   return user;
 }
+
+export async function getDbUserId() {
+  const { userId: clerkId } = await auth();
+
+  if (!clerkId) {
+    throw new Error("Unauthorized");
+  }
+
+  const user = await getUserByClerkId(clerkId);
+
+  if (!user) throw new Error("User not found");
+
+  return user.id;
+}
