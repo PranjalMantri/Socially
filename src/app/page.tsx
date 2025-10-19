3d3fd2e81641f68c1e23d8eecf5caef5f177c9ae
Pranjal Mantri <pranjalmantri@gmail.com>
2025-10-19 15:56:20 +0530

    feat: add responsive navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,3 @@
-"use client";
-
-import ModeToggle from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
-
 export default function Home() {
-  return (
-    <div>
-      <SignedOut>
-        <SignInButton mode="modal" />
-        <SignUpButton mode="modal">
-          <Button
-            variant={"secondary"}
-            className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer"
-          >
-            Sign up
-          </Button>
-        </SignUpButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-
-      <ModeToggle />
-    </div>
-  );
+  return <div>Home page content</div>;
 }
