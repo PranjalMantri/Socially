a92622a3f54e1969dcfbd6781d730e3b18e6dff5
Pranjal Mantri <pranjalmantri@gmail.com>
2025-10-20 12:25:39 +0530

    feat: add CreatePost component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import CreatePost from "@/components/CreatePost";
+
 export default function Home() {
-  return <div>Home page content</div>;
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">
+        <CreatePost />
+      </div>
+
+      <div className="hidden lg:block lg:col-span-4">Suggested Users </div>
+    </div>
+  );
 }
---
+++ src/components/CreatePost.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import { useState } from "react";
+import { Card, CardContent } from "./ui/card";
+import { useUser } from "@clerk/nextjs";
+import { Avatar, AvatarImage } from "./ui/avatar";
+import { Textarea } from "./ui/textarea";
+import { Button } from "./ui/button";
+import { ImageIcon, Loader2Icon, SendIcon } from "lucide-react";
+import { createPost } from "@/actions/post.action";
+import toast from "react-hot-toast";
+
+function CreatePost() {
+  const { user } = useUser();
+
+  const [content, setContent] = useState("");
+  const [image, setImage] = useState("");
+  const [isPosting, setIsPosting] = useState(false);
+  const [showImageUpload, setShowImageUpload] = useState(false);
+
+  const handleSubmit = async () => {
+    if (!content.trim()) return;
+
+    setIsPosting(true);
+
+    try {
+      const result = await createPost(content, image);
+
+      if (result?.success) {
+        setContent("");
+        setImage("");
+        setShowImageUpload(false);
+      }
+
+      toast.success("Post created successfully");
+    } catch (err) {
+      console.error("Failed to create a post: ", err);
+      toast.error("Failed to create post");
+    } finally {
+      setIsPosting(false);
+    }
+  };
+
+  return (
+    <Card className="mb-6">
+      <CardContent>
+        <div className="space-y-4">
+          <div className="flex space-x-4">
+            <Avatar className="w-10 h-10">
+              <AvatarImage src={user?.imageUrl || "/avatar.png"}></AvatarImage>
+            </Avatar>
+            <Textarea
+              placeholder="What's on your mind?"
+              className="min-h-[100px] resize-none border-none focus-visible:ring-0 p-2 text-base"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              disabled={isPosting}
+            />
+          </div>
+
+          <div className="flex items-center justify-between border-t pt-4">
+            <div className="flex space-x-2">
+              <Button
+                type="button"
+                variant={"ghost"}
+                size={"sm"}
+                onClick={() => setShowImageUpload(!showImageUpload)}
+                className="text-muted-foreground hover:text-primary"
+                disabled={isPosting}
+              >
+                <ImageIcon className="size-4 mr-2" />
+                Photo
+              </Button>
+            </div>
+            <Button
+              className="flex items-center"
+              onClick={handleSubmit}
+              disabled={(!content.trim() && !image) || isPosting}
+            >
+              {isPosting ? (
+                <>
+                  <Loader2Icon className="size-4 mr-2 animate-spin" />
+                  Posting...
+                </>
+              ) : (
+                <>
+                  <SendIcon className="size-4 mr-2" />
+                  Post
+                </>
+              )}
+            </Button>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default CreatePost;
